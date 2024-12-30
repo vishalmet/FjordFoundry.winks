b08233cda7dade8a33562c76b21c8573126109dd
vishalmet <vishalmet003@gmail.com>
2024-12-30 22:33:04 +0530

    responsive

--- src/components/SaleStatus.jsx
+++ src/components/SaleStatus.jsx
@@ -36,53 +36,53 @@ const SaleStatus = ({ targetDate }) => {
     }, [targetDate]);
 
     return (
-        <div className='relative w-[240px] sm:w-[430px] bg-[#30218A]/70 h-fit border-2 border-[#433099] rounded-[25px]'>
-            <div className="p-6">
+        <div className='relative w-[260px] sm:w-[430px] bg-[#30218A]/70 h-fit border-2 border-[#433099] rounded-[25px]'>
+            <div className="p-1 sm:p-6">
                 {/* Timer section remains same */}
-                <div className="flex justify-between text-[#B4A3F8]">
+                <div className="flex justify-between text-[#B4A3F8] text-sm sm:text-base">
                     <p>Sale Status</p>
                     <p>Ends In</p>
                 </div>
-                <div className="flex justify-between items-baseline pt-2">
+                <div className="flex justify-between items-baseline sm:pt-2">
                     <p className='flex items-center gap-2 font-medium'>
                         <FaPlay className='text-[#24FF95]' /> Live Now
                     </p>
                     <div className="flex justify-between">
                         <div className="flex gap-2">
                             {/* Timer display remains same */}
-                            <p className='text-center'>
+                            <p className='text-center text-xs sm:text-base'>
                                 {String(timeLeft.days).padStart(2, '0')}
-                                <p className='text-[#B4A3F8] text-sm'>days</p>
+                                <p className='text-[#B4A3F8] text-xs sm:text-sm'>days</p>
                             </p>
                             <p className='text-base'>/</p>
-                            <p className='text-center'>
+                            <p className='text-center text-xs sm:text-base'>
                                 {String(timeLeft.hours).padStart(2, '0')}
-                                <p className='text-[#B4A3F8] text-sm'>hours</p>
+                                <p className='text-[#B4A3F8] text-xs sm:text-sm'>hours</p>
                             </p>
-                            <p className='text-center'>
+                            <p className='text-center text-xs sm:text-base'>
                                 {String(timeLeft.minutes).padStart(2, '0')}
-                                <p className='text-[#B4A3F8] text-sm'>mins</p>
+                                <p className='text-[#B4A3F8] text-xs sm:text-sm'>mins</p>
                             </p>
-                            <p className='text-center'>
+                            <p className='text-center text-xs sm:text-base'>
                                 {String(timeLeft.seconds).padStart(2, '0')}
-                                <p className='text-[#B4A3F8] text-sm'>secs</p>
+                                <p className='text-[#B4A3F8] text-xs sm:text-sm'>secs</p>
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="bg-gradient-to-b from-[#4027A7] to-[#352194] rounded-[25px] h-fit p-6 space-y-3">
-                <p className='text-xl'>Buy IOURKFI</p>
-                <div className="bg-[#30218A]/70 h-fit rounded-[20px] border-2 border-[#433099] p-4">
+            <div className="bg-gradient-to-b from-[#4027A7] to-[#352194] rounded-[25px] h-fit p-1 sm:p-6 space-y-1 sm:space-y-3">
+                <p className='text-xs sm:text-xl'>Buy IOURKFI</p>
+                <div className="bg-[#30218A]/70 h-fit rounded-[20px] border-2 border-[#433099] p-1 sm:p-4 text-xs sm:text-base">
                     <p className='text-[#B4A3F8]'>Collateral</p>
                     <div className="flex justify-between">
                         <input type="text" placeholder='0' className='bg-transparent outline-none' />
                         <button 
                             onClick={() => setShowTokenModal(true)}
-                            className="font-medium bg-[#6575FF] p-2 px-4 w-fit rounded-full flex gap-1 items-center"
+                            className="font-medium bg-[#6575FF] p-2 sm:px-4 w-fit rounded-full flex gap-1 items-center"
                         >
-                            <FaAngleDown className='size-4' />
+                            <FaAngleDown className='size-3 sm:size-4' />
                             {selectedToken}
                         </button>
                     </div>
@@ -104,20 +104,20 @@ const SaleStatus = ({ targetDate }) => {
                     </div>
                 )}
 
-                <div className="bg-[#30218A]/70 h-fit rounded-[20px] border-2 border-[#433099] p-4">
-                    <div className="text-[#B4A3F8] flex justify-between">
+                <div className="bg-[#30218A]/70 h-fit rounded-[20px] border-2 border-[#433099] p-1 sm:p-4">
+                    <div className="text-[#B4A3F8] flex justify-between text-xs sm:text-base">
                         <p>Project Tokens</p>
                         <p>Max</p>
                     </div>
-                    <div className="flex justify-between">
+                    <div className="flex justify-between text-xs sm:text-base">
                         <input type="text" disabled placeholder='0' className='bg-transparent outline-none' />
                         <div className="font-semibold pt-2 flex gap-1 items-center">
                             IOURKFI
-                            <img src={FjordLogo} className='w-8 h-8 rounded-full' alt="" />
+                            <img src={FjordLogo} className='w-3 h-3 sm:w-8 sm:h-8 rounded-full' alt="" />
                         </div>
                     </div>
                 </div>
-                <button className='w-full bg-gradient-to-b from-[#665EFF] to-[#8C85FF] border border-[#4E3ABA] p-3 rounded-full font-semibold opacity-80 mt-2 cursor-not-allowed' disabled>Connect Wallet to Swap</button>
+                <button className='w-full bg-gradient-to-b from-[#665EFF] to-[#8C85FF] border border-[#4E3ABA] p-1 sm:p-3 text-sm sm:text-base rounded-full font-semibold opacity-80 sm:mt-2 cursor-not-allowed' disabled>Connect Wallet to Swap</button>
             </div>
         </div>
     )
--- src/components/TokenEvents.jsx
+++ src/components/TokenEvents.jsx
@@ -33,54 +33,54 @@ const TokenEvents = ({ onViewNow, targetDate }) => {
     }, [targetDate]);
 
     return (
-        <div className='w-[240px] sm:w-[430px] bg-[#1A0E44]/70 h-fit border-2 border-[#2A1F62] rounded-[25px] p-6'>
+        <div className='w-[260px] sm:w-[430px] bg-[#1A0E44]/70 h-fit border-2 border-[#2A1F62] rounded-[25px] p-1 sm:p-6'>
             <div className="flex items-center justify-between">
                 <div className="">
-                    <p className='text-2xl font-semibold'>ArkeFi</p>
-                    <p className='text-[#B4A3F8]'>$IOURKFI</p>
+                    <p className='text-base sm:text-2xl font-semibold'>ArkeFi</p>
+                    <p className='text-sm sm:text-base text-[#B4A3F8]'>$IOURKFI</p>
                 </div>
-                <img src={FjordLogo} className='h-16 rounded-full' alt="" />
+                <img src={FjordLogo} className='h-10 sm:h-16 rounded-full' alt="" />
             </div>
             <div className="bg-[#150837] w-full h-fit rounded-[25px] mt-6 p-4">
                 <div className="flex justify-between items-center">
                     <div className="">
-                        <p className='flex items-center gap-2 font-medium'><FaPlay className='text-[#24FF95]' /> Live Now</p>
+                        <p className='flex items-center gap-2 font-medium text-sm sm:text-base'><FaPlay className='text-[#24FF95]' /> Live Now</p>
                         <div className="flex items-center justify-between">
-                            <div className="flex gap-2">
-                                <p className='text-center'>
+                            <div className="flex gap-0 sm:gap-2">
+                                <p className='text-center text-xs sm:text-base'>
                                     {String(timeLeft.days).padStart(2, '0')} 
-                                    <p className='text-[#B4A3F8] text-sm'>days</p>
+                                    <p className='text-[#B4A3F8] text-xs sm:text-sm'>days</p>
                                 </p>
                                 <p className='text-base'>/</p>
-                                <p className='text-center'>
+                                <p className='text-center text-xs sm:text-base'>
                                     {String(timeLeft.hours).padStart(2, '0')} 
-                                    <p className='text-[#B4A3F8] text-sm'>hours</p>
+                                    <p className='text-[#B4A3F8] text-xs sm:text-sm'>hours</p>
                                 </p>
-                                <p className='text-center'>
+                                <p className='text-center text-xs sm:text-base'>
                                     {String(timeLeft.minutes).padStart(2, '0')} 
-                                    <p className='text-[#B4A3F8] text-sm'>mins</p>
+                                    <p className='text-[#B4A3F8] text-xs sm:text-sm'>mins</p>
                                 </p>
-                                <p className='text-center'>
+                                <p className='text-center text-xs sm:text-base'>
                                     {String(timeLeft.seconds).padStart(2, '0')} 
-                                    <p className='text-[#B4A3F8] text-sm'>secs</p>
+                                    <p className='text-[#B4A3F8] text-xs sm:text-sm'>secs</p>
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div className="">
-                        <div className="space-y-1 font-semibold">
-                            <p className='flex justify-end items-center text-white/80 gap-1'>94 <MdOutlinePeopleOutline className='size-6 text-white' /></p>
-                            <p className='flex items-center text-white/80 gap-1'>$509 <MdAutoGraph className='size-6 font-bold text-white' /></p>
+                        <div className="space-y-1 font-semibold text-xs sm:text-base">
+                            <p className='flex justify-end items-center text-white/80 gap-0 sm:gap-1'>94 <MdOutlinePeopleOutline className='size-3 sm:size-6 text-white' /></p>
+                            <p className='flex items-center text-white/80 gap-0 sm:gap-1'>$509 <MdAutoGraph className='size-3 sm:size-6 font-bold text-white' /></p>
                         </div>
                     </div>
                 </div>
                 <div className="border-t border-gray-500/60 my-2"></div>
                 <div className="flex items-center justify-between">
-                    <p className='text-white font-semibold'>Fixed Price</p>
-                    <HiUserGroup className='size-6 text-[#B4A3F8]' />
+                    <p className='text-white font-semibold text-sm sm:text-base'>Fixed Price</p>
+                    <HiUserGroup className='size-3 sm:size-6 text-[#B4A3F8]' />
                 </div>
             </div>
-                <button className='w-full bg-gradient-to-b from-[#665EFF] to-[#8C85FF] border border-[#4E3ABA] p-3 rounded-full font-semibold hover:opacity-80 mt-2' onClick={onViewNow}>View Now</button>
+                <button className='w-full bg-gradient-to-b from-[#665EFF] to-[#8C85FF] border border-[#4E3ABA] text-sm sm:text-base p-1 sm:p-3 rounded-full font-semibold hover:opacity-80 mt-1 sm:mt-2' onClick={onViewNow}>View Now</button>
         </div>
     )
 }
